31cd245d2338d855fbcc953ba25d3ce8202e5d1a
infinitearray <bandiharshav97@gmail.com>
2015-12-19 12:24:29 +0530

    The entered input is now recieved andd then printed

--- app/src/main/java/com/example/harsha/basicapp/DisplayMessageActivity.java
+++ app/src/main/java/com/example/harsha/basicapp/DisplayMessageActivity.java
@@ -1,16 +1,26 @@
 package com.example.harsha.basicapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.content.Intent;
+import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_message);
+       // setContentView(R.layout.activity_display_message);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textview = new TextView(this);
+        textview.setTextSize(40);
+        textview.setText("You have entered '"+message+"'");
+
+        setContentView(textview);
     }
 
     @Override
